dcbf470ef2f3974df995cdb3a8f0f1d8279acfda
mariodragun <mariodragun1982@gmail.com>
2021-01-31 15:59:32 +0000

    adding description to player1 variable

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ function start() {                  /*  main start function  */
  alert("The game has started");     /*  game started warning  */
 
  var player1 = window.prompt("Player 1, please enter your name: ");
- alert("Thanks " + player1);
+ alert("Thanks " + player1);                                            /*  player one variable  */
 
 
 
