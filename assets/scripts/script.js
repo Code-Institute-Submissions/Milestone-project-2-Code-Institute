ccdf944a9ff760a64351a060cdcdbdbc9ed4e785
mariodragun <mariodragun1982@gmail.com>
2021-01-31 16:32:06 +0000

    adding description to var pressed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,7 +9,7 @@ var player2 = window.prompt("Now, Player 2, please enter your name: ");
 alert("Thanks " + player2);                                             /*  player two variable  */
 
  alert("Press OK and Play!");
- var pressed = []; 
+ var pressed = [];                       /*  variable pressed it will be run after player1 and player2 add their names */
 
 
 
