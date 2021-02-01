b9d32208534fcbce7466c633d02fffe77be06c37
mariodragun <mariodragun1982@gmail.com>
2021-02-01 21:37:42 +0000

    clearing typos and var pressed = [];                                      /*assign array to variable with user input*/

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,51 +1,59 @@
-var pressed = [];
-var score1 = 0
-var score2 = 0
-var player1 = 0
-var player2 = 0
+var pressed = [];                                      /*assign array to variable with user input*/
+var score1 = 0;
+var score2 = 0;
+var player1 = 0;
+var player2 = 0;
+var player1choice = pressed[0];
+var player2choice = pressed[1];
 
-function start() {
-  
-  alert("The game has started");
-  
-   var player1 = window.prompt("Player 1, please enter your name: ");
-   alert("Thanks " + player1);                                           /*  player one variable  */
-     
-    var player2 = window.prompt("Now, Player 2, please enter your name: ");
-    alert("Thanks " + player2);                                            /*  player two variable  */
-  
-    alert("Press OK and Play!");
-    var pressed = [];                            /*  variable pressed it will be run after player1 and player2 add their names */              
-  
- window.addEventListener('keyup', (e) => {
-  //console.log(e.key);
-   pressed.push(e.key);
-    //console.log(pressed.length);
+function greetings() {
+    alert("The game has started");                                                 /*palyer 1 and palyer2 variables are added to function greeting*/
+     this.player1 = window.prompt("Player 1, please enter your name: ");
+     alert("Thanks " + player1);    
+     this.player2 = window.prompt("Now, Player 2, please enter your name: ");
+    alert("Thanks " + player2); 
+}
+
+ window.addEventListener('keyup', (e) => {                                    /*window global scope*/
+          //console.log(e.key);
+           pressed.push(e.key);
+          //console.log(pressed.length);
+           //console.log(pressed);
    
-   if(pressed.length == 2) {
-         alert("Let´s see who win!")                         /*  alert after both player played their choices  */
+            if(pressed.length == 2) {
+              player1choice = pressed[0] 
+              player2choice = pressed[1]
+              //console.log(player1choice, player2choice)
+                 alert("Let´s see who win!")  
+              this.winner(pressed); 
+            }
+          })
 
-         function winner(pressed){
-             if(
-               JSON.stringify(pressed) == JSON.stringify(["q", "8"]) ||
-               JSON.stringify(pressed) == JSON.stringify(["w", "9"]) ||
-               JSON.stringify(pressed) == JSON.stringify(["a", "5"]) ||
-               JSON.stringify(pressed) == JSON.stringify(["s", "6"]) ||
-               JSON.stringify(pressed) == JSON.stringify(["z", "3"])
-                ) { alert("DRAW! Refressh the page and try again!")} 
-           else if (
-             JSON.stringify(pressed) == JSON.stringify(["q", "3"]) ||
-             JSON.stringify(pressed) == JSON.stringify(["q", "5"]) ||
-             JSON.stringify(pressed) == JSON.stringify(["w", "8"]) ||
-             JSON.stringify(pressed) == JSON.stringify(["w", "6"]) ||
-             JSON.stringify(pressed) == JSON.stringify(["a", "9"]) ||
-             JSON.stringify(pressed) == JSON.stringify(["a", "3"]) ||
-             JSON.stringify(pressed) == JSON.stringify(["s", "5"]) ||
-             JSON.stringify(pressed) == JSON.stringify(["s", "8"]) ||
-             JSON.stringify(pressed) == JSON.stringify(["z", "6"]) ||
-             JSON.stringify(pressed) == JSON.stringify(["z", "9"])
-                ) 
-                switch(player1choice){
+function play() {
+  alert("Please press OK and Play!");                                        /*function play*/
+}
+
+function winner(pressed){
+      if(
+        JSON.stringify(pressed) == JSON.stringify(["q", "8"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["w", "9"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["a", "5"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["s", "6"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["z", "3"])
+      ) { alert("DRAW! Press start to play again!")} 
+      else if (
+        JSON.stringify(pressed) == JSON.stringify(["q", "3"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["q", "5"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["w", "8"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["w", "6"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["a", "9"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["a", "3"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["s", "5"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["s", "8"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["z", "6"]) ||
+        JSON.stringify(pressed) == JSON.stringify(["z", "9"])
+      ) { 
+        switch(player1choice){
           case "q":
             player1choice = "ROCK!";
             break;
@@ -61,11 +69,14 @@ function start() {
           case "z":
             player1choice = "LIZARD!";
             break;
-                }
-                {  alert(this.player2 + " WIN with " + this.player2choice)
-        this.score2++
+        }
+
+
+        alert(this.player1 + " WIN with " + this.player1choice)
+        this.score1++
         alert(this.player1 + ": " + this.score1 + " X " + this.player2 + ": " + this.score2)
-      }
+
+      } 
       else {
         switch(this.player2choice){
           case "8":
@@ -84,26 +95,27 @@ function start() {
             this.player2choice = "LIZARD!";
             break;
         }
+        alert(this.player2 + " WIN with " + this.player2choice)
+        this.score2++
+        alert(this.player1 + ": " + this.score1 + " X " + this.player2 + ": " + this.score2)
+      }
       keepGoing();
-                  }
-              }
-            }
-                          winner(pressed);
-    }  
-  
+
+}      
     
-    //console.log(pressed); 
+    //console.log(pressed);
+   
+  
 
 
-
-   function keepGoing() {
+function keepGoing() {                                                     
    if(confirm('Do you want to continue?')) { 
      this.pressed = [];
      play()
    } else {alert("Game Over!")}
-   
-function start() {
+}
+
+function start() {                                                      /* function start calls function greetings and function play to begin game*/
      greetings();
       play();
 }
-  )}
